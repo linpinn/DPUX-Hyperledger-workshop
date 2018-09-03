55314ec83e48ca559fa293a100164e18d0ab844f
Alongkorn Chetasumon <draood@gmail.com>
2018-09-03 23:44:40 +0700

    clean up

--- 3_HLF deployment/application/src/controllers/createCar.js
+++ 3_HLF deployment/application/src/controllers/createCar.js
@@ -3,13 +3,13 @@ const { invoke } = require('../service')
 const createCar = async (req, res) => {
   try {
     const enrollmentID = req.user
-    const queryOptions = {
+    const options = {
       chaincodeId: 'fabcar',
       fcn: 'createCar',
       args: ['CAR13', 'dfdfdfdf', 'ddd', 'Red', 'Nick3'],
       chainId: 'mychannel',
     }
-    const result = await invoke(enrollmentID, queryOptions)
+    const result = await invoke(enrollmentID, options)
     res.json(result)
   } catch(e) {
     res.status(500).json({ error: e.message })
--- 3_HLF deployment/application/src/controllers/getAllCars.js
+++ 3_HLF deployment/application/src/controllers/getAllCars.js
@@ -3,12 +3,12 @@ const { query } = require('../service')
 const getCars = async (req, res) => {
   try {
     const enrollmentID = req.user
-    const queryOptions = {
+    const options = {
       chaincodeId: 'fabcar',
       fcn: 'queryAllCars',
       args: ['']
     }
-    const result = await query(enrollmentID, queryOptions)
+    const result = await query(enrollmentID, options)
     res.json(result)
   } catch(e) {
     res.status(500).json({ error: e.message })
