bde1c9b0f645e4ac9d6895ce3f2d2d28f2706932
Alongkorn Chetasumon <draood@gmail.com>
2018-09-03 23:09:20 +0700

    clean app.js

--- 3_HLF deployment/application/src/app.js
+++ 3_HLF deployment/application/src/app.js
@@ -1,27 +1,14 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const routes = require('./routes')
 
 const app = express()
 const port = process.env.PORT || '3003'
 
-const { eventHub, register, enroll } = require('./service/index')
-
-const Options = require('./util/helper')
-const routes = require('./routes');
-
-const options = new Options()
-
 const startApp = () => {
-  // for eventhub testing purpose
-  // eventHub()
-  // register({ enrollmentID: 'usertest2'})
-
-  // enroll(options.enrollment)
-
   app.use(bodyParser.urlencoded({ extended: true }))
   app.use(bodyParser.json())
   app.use(routes)
-
   app.listen(port, () => {
     console.log(`app is listening on ${port}`)
   })
