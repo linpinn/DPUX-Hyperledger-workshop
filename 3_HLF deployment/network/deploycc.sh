5756afc478a9d7d126407978edd174670be3e2cf
Kriengsak Ko <kriengsak.ko@kbtg.tech>
2018-09-05 00:12:27 +0700

    add echo step by step and modify upgrade chaincode script

--- 3_HLF deployment/network/deploycc.sh
+++ 3_HLF deployment/network/deploycc.sh
@@ -5,24 +5,29 @@ CC_SRC_PATH=github.com/demo
 LANGUAGE="golang" # node if chaincode written by nodejs
 
 # clear old chaincode images
+echo "clear old chaincode images .."
 docker rmi $(docker images | grep dev-peer | awk '{print $3}')
 
 # copy chaincode path to chaincode in fabric-samples
+echo "copy chaincode to docker mounted path - fabric-samples .."
 rm -Rf ../fabric-samples/chaincode/demo
 cp -Rf demo ../fabric-samples/chaincode
 ls ../fabric-samples/chaincode | grep demo
 
 # start fabric network with cli
+echo "start hyperledger fabric network .."
 cd ../fabric-samples/basic-network
 ./start.sh
 docker-compose up -d cli
 cd -
 
 # install chaincode
+echo "install chaincode .."
 docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp" cli peer chaincode install -n demo -v 1.0 -p "$CC_SRC_PATH" -l "$LANGUAGE"
 
 # instantiate chaincode
 # -P is endorsement policy
+echo "instantiate chaincode .."
 docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp" cli peer chaincode instantiate -o orderer.example.com:7050 -C mychannel -n demo -l "$LANGUAGE" -v 1.0 -c '{"Args":[""]}' -P "OR ('Org1MSP.member','Org2MSP.member')"
 sleep 3
 
