e21390219e3d675e31891c5d463294071d731313
Kriengsak Ko <kriengsak.ko@kbtg.tech>
2018-09-06 16:54:36 +0700

    add config for basic-network and prerequisite script

---
+++ 5_Extra/config/prerequisite.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# copy file
+cp appconfig.json ../blockchain-explorer/.
+cp config.json ../blockchain-explorer/app/platform/fabric/.
+cp package.json ../blockchain-explorer/client/.
+echo "copy configuration completed !!"
+
+# npm install
+cd ../blockchain-explorer
+npm i
+cd ./app/test
+npm i
+sleep 5s
+npm test
+cd -
+cd ./client
+npm i
+sleep 5s
+npm run build
+cd -
+
+# install postgresql
+sudo apt-get install postgresql postgresql-contrib
+cd ./app/persistence/postgreSQL/db
+./createdb.sh 
+
+echo "prerequisite completed !!"
+
